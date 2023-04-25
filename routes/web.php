700aefb963b48d21e41e6de740d34962d9851e12
Nitesh Das <39939643+niteshdass@users.noreply.github.com>
2023-04-26 01:01:08 +0600

    create post

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Category;
 use App\Comment;
 use App\Post;
 use App\Tag;
+use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -19,98 +20,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+   // Create post 
+   $user_id = 1;
+
+   $category_id = 1;
+
+   $post = new Post();
    
-    // $tags = Tag::select('id', 'name')->orderByDesc(
-    //     DB::table('post_tag')
-    //     ->selectRaw('count(tag_id) as tag_count')
-    //     ->whereColumn('tags.id', 'post_tag.tag_id')
-    //     ->orderBy('tag_count', 'desc')
-    //     ->limit(1)
-    // )->get();
+   $post->title = 'post_title';
 
-    // $tags = DB::table('tags')
-    //         ->selectRaw('name, COUNT(post_tag.tag_id) as tag_count')
-    //         ->leftJoin('post_tag', 'tags.id', '=', 'post_tag.tag_id')
-    //         ->groupBy('tags.id')
-    //         ->orderByDesc('tag_count')
-    //         ->get();
-            
+   $post->content = 'post_content';
+    // if there has belongs to relationship and you want to insert the value in same time, then you should follow the following rules
+   $post->category()->associate($category_id);
+    // if there has many to relationship and you want to insert the value in same time, then you should follow the following rules
+   $result = User::find($user_id)->posts()->save($post);
 
-    // dd($tags->toArray());
-
-    // $posts = Post::select('id', 'title')->latest()->take(5)->withCount('comments')->get();
-    // dd($posts->toArray());
-    
- //! with out relationship
-
-    // $posts = DB::table('posts')
-    // ->selectRaw('posts.*, COUNT(comments.post_id) as count_comments')
-    // ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-    // ->groupBy('posts.id')
-    // ->orderByDesc('count_comments')
-    // ->get();
-    //! If has relationship than use this type of query
-    // $posts = Category::select('id', 'title')
-    //         ->withCount('comments')
-    //         ->orderByDesc('comments_count')
-    //         ->get();
-
-    // $posts = Category::with(['comments' => function($query) {
-    //     $query->select('title');
-    // }])->get();
-
-    $content = 'aliquam*';
-    $sortBy = 'user_id, title';
-    $sortByMostCommented = true;
-    $filterByUserId = null;
-    $filterByHighRatting = true;
-
-
-    // $posts = Post::select('title', 'content')
-    // ->where('title', 'like', "%$title%")
-    // ->orWhere('content', 'like', "%$content%")
-    // ->get();
-
-    // Make full text index field
-    $posts = DB::table('posts')
-            ->whereRaw("MATCH(title, content) AGAINST(? IN BOOLEAN MODE)", [$content]);
-    $posts->when($filterByHighRatting, function($q, $filterByHighRatting) {
-        return $q->whereExists(function($query) {
-            return $query->select('*')
-            ->from('comments')
-            ->whereColumn('comments.post_id', 'posts.id')
-            ->where('comments.content', 'like', '%Molestiae%')
-            ->limit(1);
-        });
-    });  
-    $posts->when($filterByUserId, function ($q, $filterByUserId) {
-        return $q->where('user_id', $filterByUserId);
-    });
-    $posts->when($sortBy, function ($query, $sortBy) {
-        return $query->orderByRaw($sortBy);
-    }, function ($query) {
-        //! It only call when $sortBy will be null or empty
-        return $query->orderByRaw('created_at', 'desc');
-    });
-    $posts->when($sortByMostCommented, function($q) {
-        return $q->orderByDesc(
-            DB::table('comments')
-            ->selectRaw('count(comments.post_id)')
-            ->whereColumn('comments.post_id', 'posts.id')
-            ->orderByRaw('count(comments.post_id) DESC')
-            ->limit(1)
-        );
-    });
-    $posts = $posts->paginate(10);
-
-        //!  there are several options you can use with the AGAINST clause in a full-text search query in Laravel
-        // IN NATURAL LANGUAGE MODE
-        // IN BOOLEAN MODE
-
-    dd($posts);
-
-
-
+   dd($result);
 
     return view('welcome');
 });
